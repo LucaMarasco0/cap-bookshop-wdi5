22ed77f10873294497e2482c6003e8bb682d3114
Mattia Zanotti <my.zan@libero.it>
2025-07-25 15:20:17 +0000

    first commit

--- app/admin-books/webapp/test/wdi5/specs/bookshop.test.js
+++ app/admin-books/webapp/test/wdi5/specs/bookshop.test.js
@@ -2,5 +2,7 @@ const ManageBooksPage = require("../pageobjects/manageBooks.page");
 const NewBookPage = require("../pageobjects/newBook.page");
 
 describe("Manage bookshop", () => {
+    it("create a new book", async () => {
 
+    });
 });
